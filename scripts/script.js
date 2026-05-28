18ab2b20c55dc65bb99d04d0ecff99c7245a9726
Danicole Lim <limdanicole@gmail.com>
2026-05-28 11:31:37 +1000

    changed js layout and fixed email form validation

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,24 @@
 /* cart interactivity */
 
-const products = [
-    {id: 1, name: "Asics Gel Dedicate 8 Rose Tennis Shoe", price: 139.95}, 
-    {id: 2, name: "Babolat Pure Drive 6 Pack 2025", price: 229.95}
-];
+// const products = [
+//     {id: 1, name: "Asics Gel Dedicate 8 Rose Tennis Shoe", price: 139.95}, 
+//     {id: 2, name: "Babolat Pure Drive 6 Pack 2025", price: 229.95}
+// ];
+
+const products = {
+    "asics": {id: 1, name: "Asics Gel Dedicate 8 Rose Tennis Shoe", price: 139.95}, 
+    "babolat": {id: 2, name: "Babolat Pure Drive 6 Pack 2025", price: 229.95}
+};
+
+function saveToCart(key) {
+    const cartList = localStorage.getItem("cartList") | []
+    localStorage.setItem("cartList", cartList.push(products.get(key)))
+
+    getSize()
+}
+
+products.get("asics")
+
 
 localStorage.setItem("Product", JSON.stringify(products));
 
@@ -32,58 +47,52 @@ function getValidation() {
         alert("Please fill in your 'First Name'");
         return false;
     }
-    localStorage.setItem("First Name", firstName);
 
     let lastName = document.forms["checkout-form"]["lname"].value;
     if (lastName == "") {
         alert("Please fill in your 'Last Name'");
         return false;
     } 
-    localStorage.setItem("Last Name", lastName);
 
     let phoneNum = document.forms["checkout-form"]["pnumber"].value;
     if (phoneNum.length != 10) {
         alert("Please enter a valid 'Phone Number'");
         return false;
     }
-    localStorage.setItem("Phone Number", phoneNum);
-
-    /* need to use regex for email and address? and exp/date? */
 
     let email = document.forms["checkout-form"]["email"].value;
     if (email == "") {
         alert("Please enter a valid 'Email'");
         return false;
     }
-    localStorage.setItem("Email", email);
+    if (!email.contains("@") || !email.contains(".")) {
+        alert("Please enter a valid 'Email'");
+        return false;
+    }
 
     let address = document.forms["checkout-form"]["address"].value;
     if (address == "") {
         alert("Please fill in your 'Address'");
         return false;
     } 
-    localStorage.setItem("Address", address);
 
     let getState = document.getElementById("state");
     if (getState.value == "select") {
         alert("Please select a 'State'");
         return false;
-    } 
-    localStorage.setItem("State", getState);
+    }
 
     let postalCode = document.forms["checkout-form"]["pcode"].value;
     if (postalCode.length != 4) {
         alert("Please enter a valid 'Postal Code'");
         return false;
     }
-    localStorage.setItem("Postal Code", postalCode);
 
     let cardNum = document.forms["checkout-form"]["cnumber"].value;
     if (cardNum.length != 16) {
         alert("Please enter a valid 'Card Number'");
         return false;
     }
-    localStorage.setItem("Card Number", cardNum);
 
     /* need to add date validation */
 
@@ -92,5 +101,4 @@ function getValidation() {
         alert("Please enter a valid 'CVC'");
         return false;
     }
-    localStorage.setItem("CVC", getCvc);
 }
