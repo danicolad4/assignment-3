f1dfe912ccd37f3d218c2eebdc8530f01df5b808
Danicole Lim <limdanicole@gmail.com>
2026-06-15 21:35:50 +1000

    added working quantity cues and adding up cart total; continuous working bc everything needs to be done ASAP

--- scripts/script.js
+++ scripts/script.js
@@ -1,8 +1,8 @@
 // cart interactivity
 
 const products = [
-     {id: 1, name: "Asics Gel Dedicate 8 Rose Tennis Shoe", price: 139.95, img: "assets/main_tennis_product.jpg"}, 
-     {id: 2, name: "Babolat Pure Drive 6 Pack 2025", price: 229.95, img: "assets/main_bags_product.jpg"}
+     {id: 1, name: "Asics Gel Dedicate 8 Rose Tennis Shoe", price: 139.95, img: "assets/pink_and_white_shoe.jpg"}, 
+     {id: 2, name: "Babolat Pure Drive 6 Pack 2025", price: 229.95, img: "assets/blue_backpack.jpg"}
  ];
 
 // add to cart
@@ -18,12 +18,59 @@ function getSize() {
 }
 
 function addToCart(productId) {
-     
     let product = {id: productId, size: getSize(), quantity: 1};
     if (product.size != 'select') {
-        cart.push(product);
+        let existing = cart.find(({id, size}) => (id == product.id && size == product.size));
+        if (existing) {
+            existing.quantity++;
+        } else {
+            alert("Your product has been added!")
+            cart.push(product);
+        }
     }
     updateCart();
+}
+
+function minusQuantity(id, size) {
+    for (let i = 0; i < cart.length; i++) {
+        if (cart[i].id == id && cart[i].size == size) {
+            if (cart[i].quantity > 1) {
+                cart[i].quantity--;
+                break;
+            } else if (cart[i].quantity == 1) {
+                cart.splice(i, 1);
+                break;
+            }
+        }
+    }
+    updateCart();
+    displayCart();
+}
+
+function plusQuantity(id, size) {
+    for (let i = 0; i < cart.length; i++) {
+        if (cart[i].id == id && cart[i].size == size) {
+            cart[i].quantity++;
+            break;
+        }
+    }
+    updateCart();
+    displayCart();
+}
+
+function removeProduct(productId, size) {
+    cart = cart.filter((product) => (product.id != productId || product.size != size));
+    updateCart();
+    displayCart();
+}
+
+function cartTotal() {
+    let cartTotal = 0.0;
+    cart.forEach((product) => {
+        let extraInfo = products.find(({id}) => id == product.id);
+        cartTotal += product.quantity * extraInfo.price;
+    });
+    return cartTotal;
 }
 
 // saving to local storage
@@ -41,34 +88,48 @@ function loadCart() {
     displayCart();
 }
 
+// after filling in payment details
+
+function clearCart() {
+    cart = [];
+    updateCart();
+}
+
 // products shown in cart
 
 function displayCart() {
     const displayedProductsEl = document.querySelector(".product-count");
-    cart.forEach((product) => {
-        let extraInfo = products.find(({id}) => id == product.id);
-        displayedProductsEl.insertAdjacentHTML("beforeend", `<div class="added-product">
-                    <img src="${extraInfo.img}" alt="${extraInfo.name}">
-                    <div class="product-info">
-                        <span><h2>${extraInfo.name}</h2></span>
-                        <h2>$${extraInfo.price}</h2>
-                        <span><h2>Size: ${product.size}</h2></span>
-                        <div class="quantity-cue">
-                            <button id="decrease">-</button>
-                            <h3 class="quantity">${product.quantity}</h3>
-                            <button id="increase">+</button>
+    if (displayedProductsEl != null) {
+        displayedProductsEl.innerHTML = '';
+        cart.forEach((product) => {
+            let extraInfo = products.find(({id}) => id == product.id);
+            displayedProductsEl.insertAdjacentHTML("beforeend", `<div class="added-product">
+                        <img src="${extraInfo.img}" alt="${extraInfo.name}">
+                        <div class="product-info">
+                            <span><h2>${extraInfo.name}</h2></span>
+                            <h2>$${extraInfo.price}</h2>
+                            <span><h2>Size: ${product.size}</h2></span>
+                            <div class="quantity-cue">
+                                <button id="decrease" onclick="minusQuantity(${product.id}, ${product.size})">-</button>
+                                <h3 class="quantity">${product.quantity}</h3>
+                                <button id="increase" onclick="plusQuantity(${product.id}, ${product.size})">+</button>
+                            </div>
+                        </div>
+                        <div class="delete-product">
+                            <img src="assets/trash-solid-full.svg" alt="Delete Product" onclick="removeProduct(${product.id}, '${product.size}')">
                         </div>
                     </div>
-                    <div class="delete-product">
-                        <img src="assets/trash-solid-full.svg" alt="Delete Product">
-                    </div>
-                </div>
-            <hr>
-        `)  
-    });
+                <hr>
+            `)  
+        });
+    }
+    const totalPrice = document.querySelector(".total-price");
+    if (totalPrice != null) {
+        totalPrice.textContent = `$${cartTotal().toFixed(2)}`;
+    }
 }
 
-document.addEventListener("DOMContentLoaded", loadCart());
+    document.addEventListener("DOMContentLoaded", loadCart());
 
 
 // form validation
@@ -129,7 +190,7 @@ function getValidation() {
     }
 
     let getYear = document.forms["checkout-form"]["year"].value;
-    if (getYear == "" || getYear < 2026) {
+    if (getYear == "" || getYear < 26) {
         alert("Please enter a valid 'Year'")
         return false;
     }
@@ -139,4 +200,7 @@ function getValidation() {
         alert("Please enter a valid 'CVC'");
         return false;
     }
+
+    clearCart();
+    return true;
 }
